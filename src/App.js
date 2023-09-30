d40ada4fb31875c17feb83e7a13918b77c51be31
sirine512 <dkhil.cyrine@gmail.com>
2023-09-30 14:53:49 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import './App.css' 
+
+const MapContainer = () => {
+  const mapStyles = {
+    height: '500px',
+    width: '75%',
+  };
+
+  const defaultCenter = {
+    address: 'Gabès,Tunisia',
+    lat:33.862006192071,
+    lng: 10.081802179097362,
+  };
+
+  return (
+    <div align="center" className='all'>
+        <h2 align="center">
+          Welcome to Our Location
+        </h2>
+
+        <LoadScript googleMapsApiKey="AIzaSyCKDw9izIJrR3X4P2BBtw_x-mUfMzfsWD8" >
+          <GoogleMap
+            mapContainerStyle={mapStyles}
+            zoom={11}
+            center={defaultCenter}
+          ></GoogleMap>
+        </LoadScript>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer />
     </div>
   );
 }
